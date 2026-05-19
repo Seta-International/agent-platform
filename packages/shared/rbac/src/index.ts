f8b1a5d366777c48c7d2638c7fe865c18b725d6d
Canh Ta <canh.ta@seta-international.vn>
2026-05-19 16:57:42 +0700

    chore(shared/rbac): scaffold package with type surface

---
+++ packages/shared/rbac/src/index.ts
@@ -0,0 +1,9 @@
+export type {
+  Permission,
+  PermissionDefinition,
+  RoleDefinition,
+  SessionScope,
+  VisibilityGate,
+} from './types.ts';
+export { perm } from './types.ts';
+export { passesGate } from './visibility.ts';
