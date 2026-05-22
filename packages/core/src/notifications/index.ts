20791e792dee74fc7f22e0c1170e7e3b229804f8
Canh Ta <canh.ta@seta-international.vn>
2026-05-22 11:40:03 +0700

    feat(core): add core.notifier subscriber with idempotent fan-out

--- packages/core/src/notifications/index.ts
+++ packages/core/src/notifications/index.ts
@@ -1,2 +1,3 @@
 export * from './events.ts';
 export { type RequestNotificationInput, requestNotification } from './request.ts';
+export { coreNotifierSubscriber, NOTIFY_CHANNEL } from './subscriber.ts';
